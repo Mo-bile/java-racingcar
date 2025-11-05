f2e3faecd4f99e61ffae94d5a64e886d7845ed22
모재영 <mo@utransfer.com>
2025-11-05 11:13:32 +0900

    string test 에 review 부분 반영

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -14,30 +14,20 @@ public class StringTest {
         // when & then
         assertThat(result).contains("1","2");
 
-        //given
-        String[] result2 = "1".split(",");
-        // when & then
-        assertThat(result2).contains("1");
+        assertThat("1".split(",")).contains("1");
     }
 
     @Test
     @DisplayName("요구사항 2: substring")
     void subStringTest() {
-        //given
-        String result = "(1,2)".substring(1, 4);
-        // when & then
-        assertThat(result).contains("1,2");
+        assertThat("(1,2)".substring(1, 4)).contains("1,2");
     }
 
     @Test
     @DisplayName("charAt() 메소드로 특정 위치 문자 가져올 때 위치 값을 벗어나면 예외 발생 테스트")
     void charAtTest() {
-        //given
-        String s = "abc";
-
-        // when & then
         assertThatThrownBy(() -> {
-            s.charAt(3);
+            "abc".charAt(3);
         }).isInstanceOf(StringIndexOutOfBoundsException.class)
                 .hasMessage("%s","Index 3 out of bounds for length 3");
     }
