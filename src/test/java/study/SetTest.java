4f59d5985444ba29d9f42382c454f90591ef09c9
모재영 <mo@utransfer.com>
2025-11-05 11:04:04 +0900

    set test 에 review 부분 반영 완료

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -26,33 +26,20 @@ public class SetTest {
     @Test
     @DisplayName("요구사항 1 : set 의 size 활용")
     void setSizeTest() {
-        int size = numbers.size();
-
-        assertThat(size).isEqualTo(3);
+        assertThat(numbers).hasSize(3);
     }
 
-//    @Test
-//    @DisplayName("요구사항 2 : set 의 contain 메소드 활용")
-//    void setContainsTest() {
-//        assertThat(numbers.contains(1)).isTrue();
-//        assertThat(numbers.contains(2)).isTrue();
-//        assertThat(numbers.contains(3)).isTrue();
-//    }
-
-//    @Test // 제거
     @ParameterizedTest
     @DisplayName("요구사항 2 : set 의 contain 메소드 활용 / 중복 코드 제거")
     @ValueSource(ints = {1, 2, 3})
-    void isContain_numbers_in_set(int num) {
-        assertThat(numbers.contains(num)).isTrue();
+    void isContain_numbers_in_set(int input) {
+        assertThat(numbers).contains(input);
     }
 
     @ParameterizedTest
     @DisplayName("요구사항 3 : contains 메소드의 결과 값이 true 이거나 false")
     @CsvSource(value = {"1:'true'", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
-    void contains(String num, String expected) {
-        int parseInt = Integer.parseInt(num);
-        boolean parseBoolean = Boolean.parseBoolean(expected);
-        assertThat(numbers.contains(parseInt)).isEqualTo(parseBoolean);
+    void Parameterized_test(int number, boolean expected) {
+        assertThat(numbers.contains(number)).isEqualTo(expected);
     }
 }
